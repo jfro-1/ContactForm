81a690bdcb9c2ee64880d54475b81e0d3de2df02
Jaafar Osamah <jaafarosamah@Jaafars-MacBook-Pro.local>
2022-11-08 16:23:15 +0100

    upload ContactForm bestanden

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
+
+class ContactController extends Controller
+{
+    public function contact()
+    {
+        return view('contact');
+    }
+    // $imagePath = request('file');
+
+    // $file = Image::make(public_path("storage/{$imagePath}"));
+
+    // $file->save();
+
+    public function sendMail(Request $request)
+    {
+        // dd($request);
+        $details = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'mesg' => $request->decs,
+            'imageUrl' => $request->imageUrl,
+            'file' => $request->file
+        ];
+
+
+        Mail::to('jfroosama10@gmail.com')->send(new ContactMail($details));
+        return back()->with('message_send', 'we will answer you:');
+    }
+}
